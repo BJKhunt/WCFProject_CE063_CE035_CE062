c416cc621dfb44f617c44ceca6efe01562ecc179
Brijesh <bjkhuntkanbi@gmail.com>
2021-02-22 08:26:15 +0530

    added script for importing json data to sql server database

---
+++ SQLQuery3.sql
@@ -0,0 +1,5 @@
+﻿Declare @JSON varchar(max)
+SELECT @JSON=BulkColumn
+FROM OPENROWSET (BULK 'C:\Users\Brijesh\Documents\GitHub\Stock-Market-System\server\fundastock.json', SINGLE_CLOB) import
+SELECT *
+FROM OPENJSON (@JSON)
---
+++ SQLQuery4.sql
@@ -0,0 +1,23 @@
+﻿Declare @JSON varchar(max)
+SELECT @JSON=BulkColumn
+FROM OPENROWSET (BULK 'C:\Users\Brijesh\Documents\GitHub\Stock-Market-System\server\fundastock.json', SINGLE_CLOB) import
+SELECT * INTO Stock
+FROM OPENJSON (@JSON)
+WITH 
+(
+    [logo] varchar(max), 
+    [listdate] varchar(max), 
+    [country] varchar(max), 
+    [industry] varchar(max), 
+    [sector] varchar(max), 
+    [marketcap] varchar(max), 
+    [employees] varchar(max), 
+    [phone] varchar(max),
+    [ceo] varchar(max),
+    [url] varchar(max),
+    [description] varchar(max),
+    [exchange] varchar(max),
+    [name] varchar(max),
+    [symbol] varchar(max),
+    [hq_address] varchar(max)
+)
